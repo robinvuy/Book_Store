321f6156dec7423928173f541e9571c6035f00b7
Robin Vuyyuru <robinvuy@MASTUDENT-Robin-Vuyyuru-C17GMB1VQ6L4.local>
2023-03-29 16:45:41 +0100

    book seeds

---
+++ spec/book_seeds.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE books RESTART IDENTITY;
+
+INSERT INTO books (title, author_name) VALUES ('Emma', 'Jane Austin');
+INSERT INTO books (title, author_name) VALUES ('Dracula', 'Bram Stoker');
